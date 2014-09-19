a348e462c64d3c292399e6b86171923f4493aa61
angyperugini <angelo@equipnet.it>
2014-09-19 10:33:39 +0200

    Create imei.js

---
+++ www/imei.js
@@ -0,0 +1,51 @@
+cordova.define("com.equipnet.deviceimei", function(require, exports, module) { 
+
+var argscheck = require('cordova/argscheck'),
+    channel = require('cordova/channel'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec'),
+    cordova = require('cordova');
+    
+    
+    channel.createSticky('onCordovaInfoReady');
+    
+// accesso al canale nel caso di onCordovainfoReady
+
+	channel.waitForInitialization('onCordovaInfoReady');
+
+	/**
+    * rappresentazione del dispositivo e costruttore.
+    * @constructor
+    */
+    
+function DeviceImei() {
+    
+    this.imei= null;
+    
+    
+    channel.onCordovaReady.subscribe(function() {
+        me.getInfo(function(info) {
+           
+	    me.imei = info.imei;
+	    
+            channel.onCordovaInfoReady.fire();
+        },function(e) {
+            me.available = false;
+            utils.alert("[ERROR] Error initializing Cordova: " + e);
+        });
+    });
+}
+
+/**
+ * accesso alle informazioni
+ */
+Device.prototype.getInfo = function(successCallback, errorCallback) {
+    argscheck.checkArgs('fF', 'DeviceImei.getInfo', arguments);
+    exec(successCallback, errorCallback, "DeviceImeisrc", "getDeviceImei", []);
+};
+
+module.exports = new Device();
+
+});
+    
+    
