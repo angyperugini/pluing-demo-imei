009b50de7afd61ee6f513f8d1681d3b3b8c6daa5
angyperugini <angelo@equipnet.it>
2014-09-19 11:53:39 +0200

    Update imei.js

--- www/imei.js
+++ www/imei.js
@@ -1,33 +1,45 @@
-cordova.define("com.plugin.deviceimei", function(require, exports, module) { 
+cordova.define("com.example.myplugin.device", function(require, exports, module) { /*
+
 
 var argscheck = require('cordova/argscheck'),
     channel = require('cordova/channel'),
     utils = require('cordova/utils'),
     exec = require('cordova/exec'),
     cordova = require('cordova');
-    
-    
-    channel.createSticky('onCordovaInfoReady');
-    
-// accesso al canale nel caso di onCordovainfoReady
 
-	channel.waitForInitialization('onCordovaInfoReady');
+channel.createSticky('onCordovaInfoReady');
+// Tell cordova channel to wait on the CordovaInfoReady event
+channel.waitForInitialization('onCordovaInfoReady');
 
-	/**
-    * rappresentazione del dispositivo e costruttore.
-    * @constructor
-    */
-    
-function DeviceImei() {
-    
+/**
+ * This represents the mobile device, and provides properties for inspecting the model, version, UUID of the
+ * phone, etc.
+ * @constructor
+ */
+function Device() {
+    this.available = false;
+    this.platform = null;
+    this.version = null;
+    this.uuid = null;
+    this.cordova = null;
+    this.model = null;
     this.imei= null;
-    
-    
+    		
+
+    var me = this;
+
     channel.onCordovaReady.subscribe(function() {
         me.getInfo(function(info) {
-           
+            //ignoring info.cordova returning from native, we should use value from cordova.version defined in cordova.js
+            //TODO: CB-5105 native implementations should not return info.cordova
+            var buildLabel = cordova.version;
+            me.available = true;
+            me.platform = info.platform;
+            me.version = info.version;
+            me.uuid = info.uuid;
+            me.cordova = buildLabel;
+            me.model = info.model;
 	    me.imei = info.imei;
-	    
             channel.onCordovaInfoReady.fire();
         },function(e) {
             me.available = false;
@@ -37,15 +49,16 @@ function DeviceImei() {
 }
 
 /**
- * accesso alle informazioni
+ * Get device info
+ *
+ * @param {Function} successCallback The function to call when the heading data is available
+ * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
  */
 Device.prototype.getInfo = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'DeviceImei.getInfo', arguments);
-    exec(successCallback, errorCallback, "DeviceImei", "getDeviceImei", []);
+    argscheck.checkArgs('fF', 'Device.getInfo', arguments);
+    exec(successCallback, errorCallback, "Device", "getDeviceInfo", []);
 };
 
-module.exports = new DeviceImei();
+module.exports = new Device();
 
 });
-    
-    
